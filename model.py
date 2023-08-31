241fe686b0e3611f5e5a251b8e8695593f48bd38
Sadique Abdullah <62884542+sadik-abd@users.noreply.github.com>
2023-08-31 19:51:34 +0600

    Update model.py

--- model.py
+++ model.py
@@ -2,12 +2,13 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
 
-class SentenceEmbeddingModel(torch.nn.Module):
-    def __init__(self, model_name="sentence-transformers/multi-qa-MiniLM-L6-cos-v1",device="cuda"):
-        super(SentenceEmbeddingModel, self).__init__()
+class ChatModel:
+    def __init__(self,ques,ans, model_name="sentence-transformers/multi-qa-MiniLM-L6-cos-v1",device="cpu"):
         self.device = device
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.model = torch.jit.load("embeds.pt").to(device)
+        self.ques = self.encode(ques)
+        self.ans = self.encode(ans)
 
     def mean_pooling(self, model_output, attention_mask):
         token_embeddings = model_output[0]
@@ -29,15 +30,20 @@ class SentenceEmbeddingModel(torch.nn.Module):
         embeddings = F.normalize(embeddings, p=2, dim=1)
 
         return embeddings
-    def forward(self,query, docs):
-      query_emb = F.normalize(self.mean_pooling(self.model(query['input_ids'],query['attention_mask']),query['attention_mask']), p=2, dim=1)
-      doc_emb = F.normalize(self.mean_pooling(self.model(docs["input_ids"],docs["attention_mask"]),docs['attention_mask']), p=2, dim=1)
-      scores = torch.mm(query_emb, doc_emb.transpose(0, 1))
-      return scores
+    def chat(self, ques):
+      answer = ""
+      preds1 = model.predict(ques,self.ques)
+      preds2 = model.predict(ques,self.ans)
+      print(max(preds1) > max(preds2))
+      if max(preds1) > max(preds2):
+        answer = ans[torch.argmax(preds1)]
+      else:
+        answer = ans[torch.argmax(preds2)]
+      return answer
 
     def predict(self, query, docs):
         query_emb = self.encode(query)
-        doc_emb = self.encode(docs)
+        doc_emb = docs
 
         # Compute dot score between query and all document embeddings
         scores = torch.mm(query_emb, doc_emb.transpose(0, 1))[0]
